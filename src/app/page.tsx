234b1271426bcc53ac3345150ff21d870012e4a0
xDannyx <danix7123@gmail.com>
2024-10-24 20:22:09 +0200

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li>Strona główna.</li>
+          <li>O mnie.</li>
+          <li>Prowadzenie online.</li>
+          <li>Plany treningowe i diety.</li>
+          <li>Kontakt.</li>
+        </ol>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+          Made by → Daniel
+      </footer>
+    </div>
+  );
+}
