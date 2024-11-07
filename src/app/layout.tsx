0731034317651f7867c99c0ab39cd33ca5c93916
xDannyx <danix7123@gmail.com>
2024-11-07 23:18:24 +0100

    accordion, new text 'about me'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { NavigationMenuDemo } from "@/components/Navbar";
+import { NavigationMenuComponent } from "@/components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-          <NavigationMenuDemo/>
+          <NavigationMenuComponent/>
           {children}
           <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
             Made by → Daniel
